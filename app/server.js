d093715f9fa06dcf9efd95f486baa3e7963986c4
Michael Kalamogan <michaelkalamogan@gmail.com>
2021-06-13 02:50:47 +0800

    changed folder name

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 // =======================================
 //              DEPENDENCIES
 // =======================================
-require('dotenv').config()
+// require('dotenv').config()
 const express =  require('express')
 const mongoose = require('mongoose')
 const session = require('express-session')
