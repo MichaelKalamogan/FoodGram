4c1525626661373d5f75755ea195a0fe30a41c32
Michael Kalamogan <michaelkalamogan@gmail.com>
2021-06-21 21:42:51 +0800

    removed redundant else statement

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -57,56 +57,61 @@ module.exports = {
                 password2: password2
             })
 
-        } else { 
-            //if no errors in form
+            return
+
+        } 
+        
+        //if no errors in form
             
-            //checking if the user id or email already exists
-            await UserModel.findOne({
-                $or: [{user_id: user_id}, {email: email}]
-            })
-                .then(async user => {
-                    if(user) {
-                        if(user.user_id === user_id) {
-                            // inform that user id is already taken    
-                            errors.push('User Id already taken. Please use a different User Id.')
-                            res.render('register', {
-                                errors: errors,
-                                name: name,
-                                email: email,
-                            })
+        //checking if the user id or email already exists
+        await UserModel.findOne({
+            $or: [{user_id: user_id}, {email: email}]
+        })
+            .then(async user => {
+                if(user) {
+                    if(user.user_id === user_id) {
+                        // inform that user id is already taken    
+                        errors.push('User Id already taken. Please use a different User Id.')
+                        res.render('register', {
+                        errors: errors,
+                        name: name,
+                        email: email,
+                        })
 
-                        } else if (user.email === email) {
-                        // inform that email is already registered
-                            errors.push('Email is already registered. Please log in instead.')
-                            res.render('register', {
-                                errors: errors,
-                                ser_id: user_id,
-                                name: name,
-                                email: email,
-                            })
-                        
-                        }
-                    } else {
-                        //if no such user
+                    } else if (user.email === email) {
+                    // inform that email is already registered
+                        errors.push('Email is already registered. Please log in instead.')
 
-                        //Hash the password using bcrypt and saltrounds of 10
-                        const hash = bcrypt.hashSync(password, 10);
-
-                        //Register the new User
-                        UserModel.create({
+                        res.render('register', {
+                            errors: errors,
                             user_id: user_id,
                             name: name,
                             email: email,
-                            password: hash
                         })
-                            .then(user => {
+                        
+                    }
+
+                } else {
+                //if no such user
+
+                    //Hash the password using bcrypt and saltrounds of 10
+                    const hash = bcrypt.hashSync(password, 10);
+
+                    //Register the new User
+                    UserModel.create({
+                        user_id: user_id,
+                        name: name,
+                        email: email,
+                        password: hash
+                    })
+                        .then(user => {
                             req.flash('success_message', 'Successfully Registered. Please Login and visit your dashboard to update your profile.')
                             res.render('login', {success_message: req.flash('success_message')})
                                 
-                            })
-                    }
-                })         
-        }
+                        })
+                }
+            })         
+        
     },
 
     //Login the user
